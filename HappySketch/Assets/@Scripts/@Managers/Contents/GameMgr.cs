c000de9ed938088331574fdcb62daf7bb13b1900
JunHyeok Heo <67333305+godgjwnsgur7@users.noreply.github.com>
2024-11-04 09:10:10 +0900

    [준혁] 경로변경
    .

--- HappySketch/Assets/@Scripts/@Managers/Contents/GameMgr.cs
+++ HappySketch/Assets/@Scripts/@Managers/Contents/GameMgr.cs
@@ -4,5 +4,22 @@ using UnityEngine;
 
 public class GameMgr
 {
+    public BaseStage CurrStage { get; private set; }
+
+    public void Init()
+    {
+
+    }
+
+    public void Clear()
+    {
+
+    }
+
+    public void StartStage(int stageId)
+    {
+
+    }
     
+
 }
