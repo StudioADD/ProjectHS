81f0960462ef2cad436e784b119a56479ae905e5
JunHyeok Heo <67333305+godgjwnsgur7@users.noreply.github.com>
2024-11-07 16:42:34 +0900

    [준혁] 누락푸시

--- HappySketch/Assets/@Scripts/Util/Param/UIParam.cs
+++ HappySketch/Assets/@Scripts/Util/Param/UIParam.cs
@@ -19,16 +19,3 @@ public class UIFadeEffectParam : UIParam
         this.onFadeInCallBack = onFadeInCallBack;
     }
 }
-
-
-#region UI Object
-public class UIObjectParam { }
-
-public class UIStageParam : UIObjectParam { }
-
-public class UI상어피하기Param : UIStageParam 
-{
-    
-}
-
-#endregion
