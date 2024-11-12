f74d9b91262d6444bfdf5a2ebe1eadc583e24568
MomDra <yem9087@gmail.com>
2024-11-12 17:54:53 +0900

    [희용] Candy View 추가

--- HappySketch/Assets/@Scripts/UI/GameScene/Stage2/CollectCandyView.cs
+++ HappySketch/Assets/@Scripts/UI/GameScene/Stage2/CollectCandyView.cs
@@ -1,9 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CollectCandyView : ViewBase
 {
+    [SerializeField]
+    private TextMeshProUGUI minute;
+
+    [SerializeField]
+    private TextMeshProUGUI second;
+
+    [SerializeField]
+    private TextMeshProUGUI score;
+
+    [SerializeField]
+    private TextMeshProUGUI[] itemCount;
+
     
+    public void UpdatMInjute()
+    {
+           
+    }
+
+    //public v
 }
