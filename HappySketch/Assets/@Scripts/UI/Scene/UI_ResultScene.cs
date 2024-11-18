b0d825e2b8677e20de629701eafcde1f9a80b882
MomDra <yem9087@gmail.com>
2024-11-18 17:40:09 +0900

    [희용] Result 씬 구현

--- HappySketch/Assets/@Scripts/UI/Scene/UI_ResultScene.cs
+++ HappySketch/Assets/@Scripts/UI/Scene/UI_ResultScene.cs
@@ -4,12 +4,27 @@ using UnityEngine;
 
 public class UI_ResultScene : UI_BaseScene
 {
+    [SerializeField]
+    private GameObject leftPlayer;
+
+    [SerializeField]
+    private GameObject rightPlayer;
+
     public override bool Init()
     {
         if (base.Init() == false)
             return false;
 
+        Managers.UI.SetCanvasNotOverlay(gameObject, false);
+        Managers.UI.SetSceneUI(this);
+
         return true;
+    }
+
+    public void SetActivePlayer(bool isLeft)
+    {
+        leftPlayer.SetActive(isLeft);
+        rightPlayer.SetActive(!isLeft);
     }
 }
 
